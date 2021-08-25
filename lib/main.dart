d9b33e725aaba86b4ae49295bc30fd4e2c88ea81
marco <marco@cypherstack.com>
2021-08-25 11:46:16 -0600

    statefull widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion(){
-    questionIndex++;
+    setState(() {
+      questionIndex++;
+    });
     print("answer chosen!");
   }
   @override
